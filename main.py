5ed9f9c8179eba4121be7cd48d901a10378174c8
Ahmadreza <46781518+Ahmadrezadl@users.noreply.github.com>
2021-01-22 01:58:11 +0330

    add logs

--- main.py
+++ main.py
@@ -12,17 +12,31 @@ import click
 @click.option('--timeout', '-t', default=2.50, help='Request timeout time')
 def main(url, method, body, headers, count, timeout):
     start = False
+    codes = dict()
+    missed = 0
 
     def send_request():
         while not start: continue
         if method == 'get':
-            requests.get(url, headers=headers, timeout=timeout)
+            response = requests.get(url, headers=headers, timeout=timeout)
         else:
-            requests.request(method, url, headers=headers, body=body, timeout=timeout)
+            response = requests.request(method, url, headers=headers, body=body, timeout=timeout)
+        if response.status_code in codes.keys():
+            codes[response.status_code] += 1
+        else:
+            codes[response.status_code] = 1
+
     for i in range(count):
-        threading.Thread(target=send_request).start()
+        try:
+            threading.Thread(target=send_request).start()
+        except TimeoutError:
+            missed += 1
 
     start = True
+    while threading.active_count() > 1:
+        continue
+    click.echo(str(count) + " Requests sent.\nNumber of timeouts: " + str(missed) + "\nStatus codes:")
+    print(codes)
 
 
 if __name__ == "__main__":
